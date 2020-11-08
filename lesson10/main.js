93dfe6092d45faf3a6ccf7835b237575cb3bcb41
robocodebr <robocodebr@gmail.com>
2020-11-08 20:03:23 +0200

    lesson10

---
+++ lesson10/main.js
@@ -0,0 +1,44 @@
+/*
+for(i=0; i<1000;i++){
+    console.log(Math.round(Math.random()*100000));
+}
+
+
+let names = ["Тарас", "Тарас1", "Тарас2"]
+for(let i = 0; i< names.length; i++){
+    console.log(names[i])
+}
+
+//let names = ["Тарас", "Тарас2", "тАРАС3"]
+let arr2 = new Array(2)
+let arr = []
+arr[0]
+console.log(arr)
+for(let i =0; i<10; i++){
+    arr[i] = Math.floor(Math.random()*10);
+}
+console.log(arr)
+console.log(arr.length)
+
+for(let i = 0; i < names.length; i++){
+    console.log(names[i])
+}
+*/
+let prop = ["ПК", "МИШ", "КЛАВА","МОНИТОР","КОВРИК ДЛЯ МИШИ"]
+console.log(prop)
+prop[2] = 5
+console.log(prop[2])
+prop.length
+
+prop[5] = "Лалка"
+prop[6] = "Taras"
+prop[7] = "Kfkrf"
+prop[8] = "kjh"
+
+prop.pop()
+console.log(prop.length)
+prop.push("Kfkrf")
+alert(prop)
+
+prop.shift(kjh)
+prop.unshift(fgh)
---
+++ lesson9/script.js
@@ -0,0 +1,50 @@
+/*
+console.log("Helo world")
+let i = 0
+while(i>=-10){
+    console.log(i)
+    i--
+}
+
+console.log("i="+i)
+for(let t = 0 ; t < 10 ; t++){
+    console.log(t)
+    sum = t;
+}
+console.log("sum="+sum)
+
+let aver = 0;
+for(let t = 0 ; t = 20 ; t = 1){
+    aver += t;
+ 
+}
+ aver = aver/21
+ console.log("aver ="+aver)
+
+ let min = 100
+ let i = 0
+ while(i<10){
+    let r = Math.ceil(Math.random()*100)
+    if(min>r){
+        min=r
+    }
+    i++
+ }
+ console.log(min)
+ let r =0
+ do{
+     console.log(r)
+     r++
+ }while(r<10)
+ 
+let r = 45
+do{
+    console.log(r)
+    r = r+10;
+
+}while(r<=670)
+
+for(let m1 = 45){
+
+}
+*/
